5c3cde0ca050b441dbc0c80c4965f9541f0d1924
Mobius5150 <mike@michaelblouin.ca>
2016-01-19 23:29:23 -0700

    Add license headers.

--- mcp9808.c
+++ mcp9808.c
@@ -1,8 +1,22 @@
 /*
- * mcp9808.c
- *
- * Created: 2014-12-20 3:30:09 PM
- *  Author: michaelblouin
+ mcp9808.c A simple C library for the Microchip MCP9808 temperature sensor
+ By Michael Blouin (michaelblouin.ca)
+ Source available from https://github.com/Mobius5150/mcp9808
+ 
+ =========================================================================
+ Copyright 2016 Michael Blouin (michaelblouin.ca)
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */ 
 
 #include "mbtwi.h"
